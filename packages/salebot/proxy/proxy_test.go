d065281a31cd6fbb5a143dd475ec1f274a7df124
Sergei Molev <github.com@molevs.ru>
2023-10-02 15:20:01 +0300

    moving go.mod to the root of the project

--- packages/salebot/proxy/proxy_test.go
+++ packages/salebot/proxy/proxy_test.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/molevs/do-fn-salebot/mocks"
+	"github.com/molevs/do-fn-salebot/packages/salebot/proxy/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"io"
